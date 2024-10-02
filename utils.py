146fb7d7ed775bbd6ab6c0a27640e2adb8bfb10c
Madhu Mohan Reddy <128889553+ymadhumohanreddy@users.noreply.github.com>
2024-10-02 17:37:06 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 opening_text = [
-    "Cool, I'm on it mam.",
-    "Okay mam, I'm working on it.",
-    "Just a second mam.",
+    "Cool, I'm on it sir.",
+    "Okay sir, I'm working on it.",
+    "Just a second sir.",
 ]
